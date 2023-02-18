d197d7f06f123a28b2c1e7ecce6b5cb64ac6c5d4
lyly194 <dtcly.20it7@vku.udn.vn>
2023-02-18 16:29:44 +0700

    note

--- lab1/bt2.cpp
+++ lab1/bt2.cpp
@@ -1,35 +1,41 @@
 #include <iostream>
-#include <cmath>
+#include <conio.h>
+#include <math.h>
 using namespace std;
-
-int main() {
-
-    float a, b, c, x1, x2, discriminant, realPart, imaginaryPart;
-    cout << "Enter coefficients a, b and c: ";
-    cin >> a >> b >> c;
-    discriminant = b*b - 4*a*c;
-    
-    if (discriminant > 0) {
-        x1 = (-b + sqrt(discriminant)) / (2*a);
-        x2 = (-b - sqrt(discriminant)) / (2*a);
-        cout << "Roots are real and different." << endl;
-        cout << "x1 = " << x1 << endl;
-        cout << "x2 = " << x2 << endl;
+ 
+int main()
+{
+ 
+    float a, b, c, delta, x1, x2;
+    cout << "Nhap a = ";
+    cin >> a;
+    cout << "Nhap b = ";
+    cin >> b;
+    cout << "Nhap c = ";
+    cin >> c;
+    if(a == 0) {
+        // a== 0 phuong trinh tro thanh phuong trinh bac mot bx + c = 0
+        if(b == 0) {
+            if (c == 0) {
+                cout << "Phuong trinh vo so nghiem" << endl;
+            } else {
+                cout << "Phuong trinh vo nghiem" << endl;
+            }
+        } else {
+            cout << "Phuong trinh co nghiem duy nhat: " << -c/b << endl;
+        }
+    } else {
+        delta = b*b - 4*a*c;
+        if(delta > 0) {
+            x1 = (-b+sqrt(delta))/(2*a);
+            x2 = (-b-sqrt(delta))/(2*a);
+            cout << "Nghiem thu nhat x1 = " << x1 << endl;
+            cout << "Nghiem thu hai x2 = " << x2 << endl;
+        } else if ( delta == 0) {
+            cout << "Phuong trinh co nghiem kep: x1 = x2 = " << -b/2*a << endl;
+        } else {
+            cout << "Phuong trinh vo nghiem" << endl;
+        }
     }
-    
-    else if (discriminant == 0) {
-        cout << "Roots are real and same." << endl;
-        x1 = -b/(2*a);
-        cout << "x1 = x2 =" << x1 << endl;
-    }
-
-    else {
-        realPart = -b/(2*a);
-        imaginaryPart =sqrt(-discriminant)/(2*a);
-        cout << "Roots are complex and different."  << endl;
-        cout << "x1 = " << realPart << "+" << imaginaryPart << "i" << endl;
-        cout << "x2 = " << realPart << "-" << imaginaryPart << "i" << endl;
-    }
-
     return 0;
 }
