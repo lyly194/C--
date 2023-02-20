3e8031c1cd5e1e868add3fcd847811d93b746693
lyly194 <dtcly.20it7@vku.udn.vn>
2023-02-20 12:36:18 +0700

    s2

--- lab1/bt5.cpp
+++ lab1/bt5.cpp
@@ -1,112 +1,110 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-void getArrayElements(double *, const int);
-void displayArray(double *, const int);
-void selectionSort(double *, const int);
-double averageArrayElements(double *, const int);
-void swap(double *, double *);
-int inputValidate(int);
-double inputValidate(double);
+// Function prototypes
+void getData(double *, int);
+void selectionSort(double *, int);
+double getAverage(double *, int);
+void displayData(double *, int, double);
 
 int main()
 {
-    int number_of_test_scores;
+	double *Test,		// cấp phát mảng động
+		   Average;		// giữ giá trị trung bình của các điểm
+	int Scores;			// giữ số điểm
 
-    cout << "Enter the number of test scores: ";
-    number_of_test_scores = inputValidate(number_of_test_scores);
 
-    double *test_scores = new double[number_of_test_scores];
+	// lấy số điểm
+	cout << "Ban co bao nhieu cot diem? ";
+	cin  >> Scores;
 
-    cout << "Enter each test score: " << endl;
-    getArrayElements(test_scores, number_of_test_scores);
-    
-    selectionSort(test_scores, number_of_test_scores);
+	// tự dộng cấp phát mảng lớn
+	// để giữ số điểm do người dùng xác định
+	Test = new double[Scores];	// cấp phát bộ nhớ
 
-    double average = averageArrayElements(test_scores, number_of_test_scores);
+	getData(Test, Scores);
 
-    cout << "\nSorted test scores:" << endl;
-    displayArray(test_scores, number_of_test_scores);
+	selectionSort(Test, Scores);
 
-    delete [] test_scores;
-    test_scores = nullptr; // 0x0
+	Average = getAverage(Test, Scores);
 
-    cout << "\nAverage = " << average << endl;
+	displayData(Test, Scores, Average);
 
-    return 0;
+	return 0;
 }
-int inputValidate(int number)
-{
-    while (!(cin >> number))
-    {
-        cout << "Error. Enter a number: ";
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    }
-    return number;
-}
-double inputValidate(double number)
-{
-    while (!(cin >> number))
-    {
-        cout << "Error. Enter a number: ";
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    }
-    return number;
-}
-void getArrayElements(double *array, const int SIZE)
-{
-    for (int i = 0; i < SIZE; i++)
-    {
-        cout << "#" << (i + 1) << ": ";
-        // cin >> array[i];
-        // cin >> *(array + i);
-        *(array + i) = inputValidate(*(array + i));
-    }
-    
-}
-void selectionSort(double *array, const int SIZE)
-{
-    int start_index,
-        minIndex,
-        minValue;
-    
-    for (start_index = 0; start_index < (SIZE - 1); start_index++)
-    {
-        minIndex = start_index;
-        minValue = *(array + start_index);
 
-        for (int index = start_index + 1; index < SIZE; index++)
-        {
-            if (*(array + index) < minValue)
-            {
-                minValue = *(array + index);
-                minIndex = index;
-            }
-            
-        }
-        swap(*(array + minIndex), *(array + start_index));
-    }
-}
-void swap(double *a, double *b)
+//                                 lấy dữ liệu                                    *
+// Hàm này yêu cầu người dùng nhập điểm kiểm tra được lưu trữ trong một mảng. *
+// Tham số Scores giữ số điểm kiểm tra được nhập vào. *
+void getData(double *Test, int Scores)
 {
-    double *temp = a;
-    a = b;
-    b = temp;
-}
-double averageArrayElements(double *array, const int SIZE)
-{
-    double sum = 0;
+	cout << "Nhap tung diem.\n";
+	for (int i = 0; i < Scores; i++)
+	{
+		do
+		{
+			cout << "so diem #" << (i + 1) << ": ";
+			cin  >> *(Test + i);
 
-    for (int i = 0; i < SIZE; i++)
-        sum += *(array + i);
-    
-    return sum / SIZE;
+			if (*(Test + i) < 0)
+			{
+				cout << "Diem phai lon hon 0.\n"
+					 << "Nhap lai ";
+			}
+
+		} while (*(Test + i) < 0);
+	}
 }
-void displayArray(double *array, const int SIZE)
+// lựa chọnSắp xếp *
+// Hàm này thực hiện sắp xếp tăng dần trên mảng Test *
+// Tham số Scores chứa số điểm trong mảng. *
+void selectionSort(double *Test, int Scores)
+{   
+	int startscan, minIndex;
+	double minValue;
+
+	for (startscan = 0; startscan < (Scores - 1); startscan++)
+	{
+		minIndex = startscan;
+		minValue = *(Test + startscan);
+		for (int i = startscan + 1; i < Scores; i++)
+		{
+			if (*(Test + i) < minValue)
+			{
+				minValue = *(Test + i);
+				minIndex = i;
+			}
+
+		}
+		*(Test + minIndex) = *(Test + startscan);
+		*(Test + startscan) = minValue;
+	}
+} 
+// lấy trung bình *
+// Hàm này tính giá trị trung bình của các điểm được lưu trữ trong một mảng. *
+// Tham số Scores giữ số điểm cần tính trung bình. *
+double getAverage(double *Test, int Scores)
 {
-    for (int i = 0; i < SIZE; i++)
-        cout << "#" << (i + 1) << ": " << *(array + i) << endl;
-    
+	double Total;
+
+	for (int i = 0; i < Scores; i++)
+	{
+		Total += *(Test + i);
+	}
+
+	return Total / Scores;
+} 
+// hiển thị dữ liệu *
+void displayData(double *Test, int Scores, double Avg) 
+{
+	cout << "\n   Diem thi\n";
+	cout << "So diem: " << Scores << endl;
+	cout << "Diem theo thu tu tang dan:\n";
+	for (int i = 0; i < Scores; i++)
+	{
+		cout << "#" << (i + 1) << ": " << *(Test + i) << endl;
+	}
+	cout << fixed << showpoint << setprecision(2);
+	cout << "Diem trung binh: " << Avg << endl; 
 }
